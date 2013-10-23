493f81fa5bd4f4f4244a7fb8d137df968b584ff6
Daniel Dihardja <daniel.dihardja@socian.net>
2013-10-23 14:19:41 +0200

    implement the onopen, onmessage and onerror method

--- src/android/AutobahnWebSocket.java
+++ src/android/AutobahnWebSocket.java
@@ -10,56 +10,128 @@ import org.json.JSONException;
 import de.tavendo.autobahn.WebSocket;
 import de.tavendo.autobahn.WebSocketConnection;
 import de.tavendo.autobahn.WebSocketException;
-import de.tavendo.autobahn.WebSocketConnectionHandler;
 
-public class AutobahnWebSocket extends CordovaPlugin{
-	
+public class AutobahnWebSocket extends CordovaPlugin implements WebSocket.ConnectionHandler {
+
 	public static final String ACTION_CONNECT = "connect";
-	public static final String ACTION_SEND = "send";
-	
-	private CallbackContext successConnectCallback;
+	public static final String ACTION_ONOPEN = "onopen";
+	public static final String ACTION_ONMESSAGE = "onmessage";
+	public static final String ACTION_ONERROR = "onerror";
 
+	public static final String ACTION_SEND = "send";
+
+	private CallbackContext onopenConnextCallback;
+	private CallbackContext onmessageConnextCallback;
+	private CallbackContext onerrorConnextCallback;
+	
+	private WebSocket wsConnection;
+	
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		try {
-			if(ACTION_CONNECT.equals(action)) {
-				return createWSConnection(args, callbackContext);
+			if (ACTION_CONNECT.equals(action)) {
+				return actionConnect(args, callbackContext);
+			} else if (ACTION_ONOPEN.equals(action)) {
+				return actionOnOpen(args, callbackContext);
+			} else if (ACTION_ONMESSAGE.equals(action)) {
+				return actionOnMessage(args, callbackContext);
+			} else if (ACTION_ONERROR.equals(action)) {
+				return actionOnError(args, callbackContext);
+			} else if(ACTION_SEND.equals(action)) {
+				return actionSend(args, callbackContext);
 			}
 			return false;
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.err.println("Exception: " + e.getMessage());
-	        callbackContext.error(e.getMessage());
-	        return false;
-		}
-	}
-	
-	private boolean createWSConnection(JSONArray args, CallbackContext callbackContext) throws JSONException {
-		try {
-			JSONObject argObject = args.getJSONObject(0);
-			WebSocket ws = new WebSocketConnection();
-			final String wsuri = argObject.getString("wsaddress");
-			ws.connect(wsuri, new WebSocketConnectionHandler() {
-				@Override
-				public void onOpen() {
-					
-				}
-
-				@Override
-				public void onTextMessage(String payload) {
-         
-				}
-
-				@Override
-				public void onClose(int code, String reason) {
-				}
-			});
-			return true;
-		}
-		catch(WebSocketException e) {
-			System.err.println("Exception: " + e.getMessage());
-			callbackContext.error(e.getMessage());
+			if(onerrorConnextCallback != null) onerrorConnextCallback.error(e.getMessage());
 			return false;
 		}
+	}
+
+	private boolean actionConnect(JSONArray args, CallbackContext callbackContext) throws JSONException {
+		try {
+			JSONObject argObject = args.getJSONObject(0);
+			wsConnection = new WebSocketConnection();
+			final String wsuri = argObject.getString("wshost");
+			wsConnection.connect(wsuri, this);
+			return true;
+		} catch (WebSocketException e) {
+			System.err.println("Exception: " + e.getMessage());
+			if(onerrorConnextCallback != null) onerrorConnextCallback.error(e.getMessage());
+			return false;
+		}
+	}
+
+	private boolean actionOnOpen(JSONArray args, CallbackContext callbackContext) {
+		onopenConnextCallback = callbackContext;
+		return true;
+	}
+
+	private boolean actionOnMessage(JSONArray args, CallbackContext callbackContext) {
+		onmessageConnextCallback = callbackContext;
+		return true;
+	}
+
+	private boolean actionOnError(JSONArray args, CallbackContext callbackContext) {
+		onerrorConnextCallback = callbackContext;
+		return true;
+	}
+	
+	private boolean actionSend(JSONArray args, CallbackContext callbackContext) throws JSONException {
+		JSONObject argObject = args.getJSONObject(0);
+		String message = argObject.getString("wsmessage");
+		wsConnection.sendTextMessage(message);
+		return true;
+	}
+
+	/**
+	 * Fired when the WebSockets connection has been established. After this
+	 * happened, messages may be sent.
+	 */
+	public void onOpen() {
+		if(onopenConnextCallback != null) onopenConnextCallback.success();
+	}
+
+	/**
+	 * Fired when the WebSockets connection has deceased (or could not
+	 * established in the first place).
+	 * 
+	 * @param code
+	 *            Close code.
+	 * @param reason
+	 *            Close reason (human-readable).
+	 */
+	public void onClose(int code, String reason) {
+		if(onerrorConnextCallback != null) onerrorConnextCallback.error("Closed: " + code + " because of " + reason);
+	}
+
+	/**
+	 * Fired when a text message has been received (and text messages are not
+	 * set to be received raw).
+	 * 
+	 * @param payload
+	 *            Text message payload or null (empty payload).
+	 */
+	public void onTextMessage(String payload) {
+		if(onmessageConnextCallback != null) onmessageConnextCallback.success(payload);
+	}
+
+	/**
+	 * Fired when a text message has been received (and text messages are set to
+	 * be received raw).
+	 * 
+	 * @param payload
+	 *            Text message payload as raw UTF-8 or null (empty payload).
+	 */
+	public void onRawTextMessage(byte[] payload) {
+	}
+
+	/**
+	 * Fired when a binary message has been received.
+	 * 
+	 * @param payload
+	 *            Binar message payload or null (empty payload).
+	 */
+	public void onBinaryMessage(byte[] payload) {
 	}
 }
--- www/autobahnws.js
+++ www/autobahnws.js
@@ -1,7 +1,23 @@
 function AutobahnWebSocket() {
-	this.connect = function(address,successFunc, errorFunc) {
+	
+	var _this = this;
+	
+	this.onopen = function() {};
+	this.onmessage = function() {};
+	this.onerror = function() {};
+	
+	this.connect = function(host) {
+		cordova.exec(_this.onopen, _this.onerror , 'AutobahnWebSocket', 'onopen', []);
+		cordova.exec(_this.onmessage, _this.onerror , 'AutobahnWebSocket', 'onmessage', []);
+		cordova.exec(_this.onopen, _this.onerror, 'AutobahnWebSocket', 'onopen', []);
 		
-		cordova.exec(successFunc, errorFunc, 'AutobahnWebSocket', 'connect', [address]);
+		var param = {'wshost' : host};
+		cordova.exec(_this.onopenHandler, _this.errorHandler, 'AutobahnWebSocket', 'connect', [param]);
+	}
+	
+	this.send = function(message) {
+		var param = {'wsmessage':message};
+		cordova.exec(null, this.onerror, 'AutobahnWebSocket', 'send', [param]);
 	}
 }
 module.exports = AutobahnWebSocket;
