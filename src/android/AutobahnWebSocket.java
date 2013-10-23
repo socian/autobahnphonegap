0159d7713bae3107b25d4a7d614b2c29ca88ea9b
Daniel Dihardja <daniel.dihardja@socian.net>
2013-10-23 12:22:36 +0200

    added the autobahn imports

--- src/android/AutobahnWebSocket.java
+++ src/android/AutobahnWebSocket.java
@@ -6,16 +6,18 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
-/*
+
 import de.tavendo.autobahn.WebSocket;
 import de.tavendo.autobahn.WebSocketConnection;
 import de.tavendo.autobahn.WebSocketException;
 import de.tavendo.autobahn.WebSocketConnectionHandler;
-*/
+
 public class AutobahnWebSocket extends CordovaPlugin{
 	
 	public static final String ACTION_CONNECT = "connect";
 	public static final String ACTION_SEND = "send";
+	
+	private CallbackContext successConnectCallback;
 
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
--- www/autobahnws.js
+++ www/autobahnws.js
@@ -1,5 +1,6 @@
 function AutobahnWebSocket() {
 	this.connect = function(address,successFunc, errorFunc) {
+		
 		cordova.exec(successFunc, errorFunc, 'AutobahnWebSocket', 'connect', [address]);
 	}
 }
